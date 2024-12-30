6ffe6ec6b30d83b07c149eda9f1c14387811a36b
harshhh10-10 <mulchandanihoundrajmal6@gmail.com>
2024-12-30 18:24:26 +0530

    ACP2

---
+++ After class.py
@@ -0,0 +1,14 @@
+num1=1122
+print(num1)
+
+num2=125
+print(num2)
+
+num3=136
+print(num2)
+
+sum=num1+num2+num3
+print("the sum of two numbers given above is",sum)
+print("subtracting the numbers",num1 - num2 - num3)
+print("multiplying the numbers",num1*num2*num3)
+print("divinding the number",num1/num2/num3)
